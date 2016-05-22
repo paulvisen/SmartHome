1b4e8d602a999c0cf296cdbdbe2c5e41d9337ef6
jwcui <2497494446@qq.com>
2016-05-22 22:53:36 +0800

    bar chart

--- app/src/main/java/com/seu/smarthome/ui/main/HistoryActivity.java
+++ app/src/main/java/com/seu/smarthome/ui/main/HistoryActivity.java
@@ -4,12 +4,12 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.seu.smarthome.R;
 
 import java.util.ArrayList;
@@ -17,39 +17,42 @@ import java.util.Random;
 
 public class HistoryActivity extends AppCompatActivity {
 
-    private LineChart chart;
+    private BarChart chart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_history);
 
-        chart = (LineChart) findViewById(R.id.history_chart);
+        chart = (BarChart) findViewById(R.id.history_chart);
         setData();
     }
 
     void setData() {
 
         ArrayList<String> xVals = new ArrayList<>();
-        ArrayList<Entry> yVals = new ArrayList<>();
+        ArrayList<BarEntry> yVals = new ArrayList<>();
         Random random = new Random();
         for(int i = 0; i < 12; i++){
             float profix = random.nextFloat() * 10;
-            yVals.add(new Entry(profix,i));
+            yVals.add(new BarEntry(profix,i));
             xVals.add((i+1)+"月");
         }
-        LineDataSet dataSet = new LineDataSet(yVals,"value");
+        BarDataSet dataSet = new BarDataSet(yVals,"value");
         dataSet.setColor(Color.rgb(89, 199, 250));
-        dataSet.setLineWidth(1.75f);
-        dataSet.setCircleColor(Color.rgb(89, 199, 250));
-        LineData data = new LineData(xVals,dataSet);
+        BarData data = new BarData(xVals,dataSet);
 
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawGridLines(false);
 
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setAxisMinValue(0.0F);
+
         chart.setData(data);
         chart.setDescription("");
         chart.animateY(3000);
-
+        chart.setScaleEnabled(false);
+        chart.setTouchEnabled(false);
+        chart.getAxisRight().setEnabled(false);
     }
 }
